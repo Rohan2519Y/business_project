e8d7b884029e0165ef957534c849b20861d0fc18
Rohan2519Y <rohankumar17645@gmail.com>
2025-08-30 16:40:40 +0530

    Initial commit - business project frontend & backend

---
+++ business_frontend/src/Components/Header.js
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (<>
+        <div style={{ width: '100%', height: '72px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <div style={{ width: '10%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <img src="./image.png" style={{ width: '84px', height: '36px' }} />
+            </div>
+            <div style={{ width: '80%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            </div>
+            <div style={{ width: '10%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <button style={{ width: '125px', height: '44px', background: 'linear-gradient(114.44deg, #3890C9 3.14%, #2B79AC 56.54%)', outline: 'none', border: '0px solid tranparent', borderRadius: '30px' }}></button>
+            </div>
+        </div>
+    </>)
